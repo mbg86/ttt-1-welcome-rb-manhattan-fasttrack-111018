169a890c8c95f6e8ddbd8c289f7f287dfaf19b3b
Manrique Buso - Garcia <manrique.buso@gmail.com>
2018-11-10 17:29:51 +0000

    Done.

---
+++ hello_world.rb
@@ -0,0 +1,16 @@
+def hello (name)
+  puts "hello #{name}"
+end
+
+name = "manrique"
+hello("manrique")
+
+# when we run this method we want it to print "hello name"
+
+def add (arg1, arg2)
+  puts arg1 + arg2
+end
+
+add(1, 1)
+
+def add_string()
